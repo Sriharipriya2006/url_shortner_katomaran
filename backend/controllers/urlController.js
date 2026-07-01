3057bed07e905e89ff0bc9bb246e76426f702312
Sriharipriya Sivakumar <sriharipriya2006@gmail.com>
2026-07-01 13:53:36 +0530

    Initial commit

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -3,19 +3,17 @@ const { nanoid } = require("nanoid");
 
 const createShortUrl = async (req, res) => {
   try {
-   const { url, expiryDate } = req.body;
+    const { url, expiryDate } = req.body;
 
-const userId = req.user.id;
+    const userId = req.user.id;
+    const shortCode = nanoid(6);
 
-const shortCode = nanoid(6);
-
-  const result = await pool.query(
-  `INSERT INTO urls(user_id, original_url, short_code, expiry_date)
-   VALUES($1,$2,$3,$4)
-   RETURNING *`,
-  [userId, url, shortCode, expiryDate]
-
-);
+    const result = await pool.query(
+      `INSERT INTO urls(user_id, original_url, short_code, expiry_date)
+       VALUES($1,$2,$3,$4)
+       RETURNING *`,
+      [userId, url, shortCode, expiryDate]
+    );
 
     res.status(201).json({
       message: "Short URL Created",
@@ -35,10 +33,10 @@ const redirectUrl = async (req, res) => {
   try {
     const { shortCode } = req.params;
 
-   const result = await pool.query(
-  "SELECT * FROM urls WHERE short_code = $1",
-  [shortCode]
-);
+    const result = await pool.query(
+      "SELECT * FROM urls WHERE short_code = $1",
+      [shortCode]
+    );
 
     if (result.rows.length === 0) {
       return res.status(404).json({
@@ -46,22 +44,9 @@ const redirectUrl = async (req, res) => {
       });
     }
 
-    const expiryDate = result.rows[0].expiry_date;
+    const originalUrl = result.rows[0].original_url;
 
-if (
-  expiryDate &&
-  new Date(expiryDate) < new Date()
-) {
-  return res.status(400).json({
-    message: "Link Expired"
-  });
-}
-
-await pool.query(
-  "UPDATE urls SET clicks = clicks + 1 WHERE short_code = $1",
-  [shortCode]
-);
-    res.redirect(result.rows[0].original_url);
+    return res.redirect(originalUrl);
 
   } catch (error) {
     console.log(error);
@@ -70,46 +55,8 @@ await pool.query(
     });
   }
 };
-const getAllUrls = async (req, res) => {
-  try {
-    const userId = req.user.id;
 
-const result = await pool.query(
-  "SELECT * FROM urls WHERE user_id = $1 ORDER BY created_at DESC",
-  [userId]
-);
-    res.json(result.rows);
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Server Error"
-    });
-  }
-};
-const deleteUrl = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    await pool.query(
-      "DELETE FROM urls WHERE id = $1",
-      [id]
-    );
-
-    res.json({
-      message: "URL Deleted Successfully"
-    });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Server Error"
-    });
-  }
-};
 module.exports = {
   createShortUrl,
-  redirectUrl,
-  getAllUrls,
-  deleteUrl
+  redirectUrl
 };
